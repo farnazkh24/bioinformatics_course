dbb1cc83e9d233728fc1bd2e738705b895a33964
farnazkh24 <159037747+farnazkh24@users.noreply.github.com>
2024-03-07 12:06:10 +0000

    not tested: updated script to make empty dirs and READ.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+mkdir -p analysis docs data
+
+
+for my_directory in scripts analysis docs data;do
+  touch ${my-directory}/README.md
+  echo "# ${my_directory}" >> ${my_directorty}/README.md
+done
